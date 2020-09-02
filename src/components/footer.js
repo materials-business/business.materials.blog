6ad90d61880ba5946d3e964082af52e8ad5661c4
Juan Arroyave <juan@scalista.com>
2020-09-02 21:35:32 +0200

    Added links to websites

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ import { RiHeart2Line } from "react-icons/ri";
 const Footer = () => (
   <footer className="site-footer">
     <div className="container">
-      <p>Materials Business is an initiave from Scalista GmbH<span className="icon -love"><RiHeart2Line/></span> by <Link to="/">scalista.com</Link> in Collaboration with P.h.D Carlos Enrique Arroyave Posada<span className="icon -love"><RiHeart2Line/></span><Link to="/">arroyave.co</Link></p>
+      <p>Materials.Business Newsletter is an initiave from <span className="icon -love"><RiHeart2Line/></span> <Link to="https://scalista.com" target="_blank">Scalista GmbH</Link> in Collaboration with <Link to="https://arroyave.co" target="_blank">P.h.D Carlos Enrique Arroyave Posada</Link><span className="icon -love"><RiHeart2Line/></span> <Link to="https://arroyave.co" target="_blank">arroyave.co</Link></p>
     </div>
   </footer>
 )
