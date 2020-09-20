83a9c294bf890b8898cd8f8facc042971db9083a
Juan Arroyave <juan@scalista.com>
2020-09-20 20:24:55 +0200

    Fix Footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ import { RiHeart2Line } from "react-icons/ri";
 const Footer = () => (
   <footer className="site-footer">
     <div className="container">
-      <p>Materials.Business Newsletter is an initiave from <span className="icon -love"><RiHeart2Line/></span> <Link to="https://scalista.com" target="_blank">Scalista GmbH</Link> in Collaboration with <Link to="https://arroyave.co" target="_blank">P.h.D Carlos Enrique Arroyave Posada</Link><span className="icon -love"><RiHeart2Line/></span> <Link to="https://arroyave.co" target="_blank">arroyave.co</Link></p>
+      <p>Materials.Business Newsletter is an initiave from <span className="icon -love"><RiHeart2Line/></span> <Link to="https://scalista.com" target="_blank">Scalista GmbH</Link> in Collaboration with <Link to="https://arroyave.co" target="_blank">Ph.D. Carlos Arroyave</Link><span className="icon -love"><RiHeart2Line/></span> <Link to="https://arroyave.co" target="_blank">arroyave.co</Link></p>
     </div>
   </footer>
 )
