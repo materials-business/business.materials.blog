205218be10f3cb2662293ef70d3540a4b1be08fd
Juan Arroyave <juan@scalista.com>
2020-11-16 12:49:56 +0100

    REmove PH Badge

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,6 @@ const Footer = () => (
     <div className="container">
       <p>Materials.Business Newsletter is an initiave from <span className="icon -love"><RiHeart2Line/></span> <Link to="https://scalista.com" target="_blank">Scalista GmbH</Link> in Collaboration with <Link to="https://arroyave.co" target="_blank">Ph.D. Carlos Arroyave</Link><span className="icon -love"><RiHeart2Line/></span> <Link to="https://arroyave.co" target="_blank">arroyave.co</Link></p>
     </div>
-    <a href="https://www.producthunt.com/posts/materials-business-newsletter?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-materials-business-newsletter" target="_blank" rel="noreferrer"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=272698&theme=dark" alt="Materials Business Newsletter - All About Asset Integrity & Corrosion Materials Business | Product Hunt"  /></a>
   </footer>
 )
 
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -43,18 +43,15 @@ class Navigation extends React.Component {
       <ListLink key={index} to={menuItem.path}>{menuItem.title}</ListLink>
     )
     return (
-      <>
-      <a href="https://www.producthunt.com/posts/materials-business-newsletter?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-materials-business-newsletter" target="_blank" rel="noreferrer"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=272698&theme=dark" alt="Materials Business Newsletter - All About Asset Integrity & Corrosion Materials Business | Product Hunt"  /></a>
-        <nav className="site-navigation">
-          <button onClick={this.handleToggleClick} className={"menu-trigger" + (this.state.showMenu ? " is-active" : "")}>
-            <div className="icon-menu-line"><RiMenu3Line/></div>
-            <div className="icon-menu-close"><RiCloseLine/></div>
-          </button>
-          <ul>
-            {listMenuItems}
-          </ul>
-        </nav>
-      </>
+      <nav className="site-navigation">
+        <button onClick={this.handleToggleClick} className={"menu-trigger" + (this.state.showMenu ? " is-active" : "")}>
+          <div className="icon-menu-line"><RiMenu3Line/></div>
+          <div className="icon-menu-close"><RiCloseLine/></div>
+        </button>
+        <ul>
+          {listMenuItems}
+        </ul>
+      </nav>
     )
   }
 }
