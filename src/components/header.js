2ce27fce225282e147cf73e7bc658adb783262ba
materials-business <110339870+materials-business@users.noreply.github.com>
2023-02-09 08:16:13 -0800

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -6,32 +6,4 @@ const Header = ({children}) => (
   </header>
 )
 
-<?php
-$query = $_GET['query'];
-
-// Connect to the database and retrieve the search results
-$search_results = search_database($query);
-
-// Display the search results
-echo "<h2>Search Results:</h2>";
-foreach ($search_results as $result) {
-  echo "<p>" . $result . "</p>";
-}
-
-// Example function to search the database and return the results
-function search_database($query) {
-  // Connect to the database
-  $db = mysqli_connect("localhost", "username", "password", "database_name");
-
-  // Escape the query to prevent SQL injection attacks
-  $query = mysqli_real_escape_string($db, $query);
-
-  // Run the SQL query to search the database
-  $result = mysqli_query($db, "SELECT * FROM articles WHERE title LIKE '%$query%' OR content LIKE '%$query%'");
-
-  // Return the search results as an array
-  return mysqli_fetch_all($result, MYSQLI_ASSOC);
-}
-
-
 export default Header
