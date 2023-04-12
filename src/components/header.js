d607c907663113f38cca5899ff8b8a297292216c
materials-business <110339870+materials-business@users.noreply.github.com>
2023-04-12 07:41:17 -0700

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,12 @@
-import React from "react"
+import React from "react";
+import PropTypes from "prop-types";
 
-const Header = ({children}) => (
-  <header className="site-header">
-    {children}
-  </header>
-)
+const Header: React.FC<{ children: React.ReactNode }> = ({ children }) => (
+  <header className="site-header">{children}</header>
+);
 
+Header.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
-export default Header
+export default Header;
